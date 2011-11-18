dcc7b8b4e02093689fdf42551f7cbd385868f181
Zach Davis <zach@castironcoding.com>
2011-11-18 11:36:45 -0800

    [FEATURE] Moved Fluid pagination widget into CICBase so that we can more easily modify it on projects

---
+++ Classes/ViewHelpers/Widget/PaginateViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "CICBase".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+
+class Tx_Cicbase_ViewHelpers_Widget_PaginateViewHelper extends Tx_Fluid_ViewHelpers_Widget_PaginateViewHelper {
+
+	/**
+	 * @var Tx_Cicbase_ViewHelpers_Widget_Controller_PaginateController
+	 */
+	protected $controller;
+
+	/**
+	 * @param Tx_Cicbase_ViewHelpers_Widget_Controller_PaginateController $controller
+	 * @return void
+	 */
+	public function injectController(Tx_Cicbase_ViewHelpers_Widget_Controller_PaginateController $controller) {
+		$this->controller = $controller;
+	}
+}
+
+?>
