5c4912c74349187a83d5e9b2d2497e7ce16bfed3
Zach Davis <zach@castironcoding.com>
2012-03-27 10:36:01 -0700

    [FEATURE] Incorporate symfony project's yaml parser

---
+++ Classes/Service/YamlParserService.php
@@ -0,0 +1,54 @@
+<?php
+
+		/***************************************************************
+		 *  Copyright notice
+		 *  (c) 2010 Zach Davis <zach
+		 * @castironcoding.com>, Cast Iron Coding, Inc
+		 *			  Lucas Thurston <lucas@castironcoding.com>, Cast Iron Coding, Inc
+		 *  All rights reserved
+		 *  This script is part of the TYPO3 project. The TYPO3 project is
+		 *  free software; you can redistribute it and/or modify
+		 *  it under the terms of the GNU General Public License as published by
+		 *  the Free Software Foundation; either version 2 of the License, or
+		 *  (at your option) any later version.
+		 *  The GNU General Public License can be found at
+		 *  http://www.gnu.org/copyleft/gpl.html.
+		 *  This script is distributed in the hope that it will be useful,
+		 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+		 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		 *  GNU General Public License for more details.
+		 *  This copyright notice MUST APPEAR in all copies of the script!
+		 ***************************************************************/
+
+		/**
+		 * Controller for the Project object
+		 *
+		 * @version $Id$
+		 * @copyright Copyright belongs to the respective authors
+		 * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+		 */
+use \Symfony\Component\Yaml\Parser;
+
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Dumper.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Escaper.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Unescaper.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Inline.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Parser.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Yaml.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Exception/ExceptionInterface.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Exception/DumpException.php');
+include_once(t3lib_extMgm::extPath('cicbase') . '/Vendor/symfony-yaml/Exception/ParseException.php');
+
+class Tx_Cicbase_Service_YamlParserService {
+
+	/**
+	 * @param $string
+	 * @return mixed
+	 */
+	public function parseString($string) {
+		$yaml = new Parser();
+		$parsed = $yaml->parse($string);
+		return $parsed;
+	}
+
+}
