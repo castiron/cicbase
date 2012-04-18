7c82c8468f2923a01ad54109b8075458a87aa961
Peter Soots <peter@castironcoding.com>
2012-04-18 12:17:42 -0700

    [FEATURE] Added some needed TCA items to save the files properly in the DB

--- ext_tables.php
+++ ext_tables.php
@@ -15,4 +15,33 @@ if (TYPO3_MODE == 'BE') {
 	}
 }
 
+
+t3lib_extMgm::addLLrefForTCAdescr('tx_cicbase_domain_model_file', 'EXT:cicbase/Resources/Private/Language/locallang_csh_tx_cicbase_domain_model_file.xml');
+t3lib_extMgm::allowTableOnStandardPages('tx_cicbase_domain_model_file');
+$TCA['tx_cicbase_domain_model_file'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:cicbase/Resources/Private/Language/locallang_db.xml:tx_cicbase_domain_model_file',
+		'label' => 'title',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/File.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_cicbase_domain_model_file.gif'
+	),
+);
+
+
 ?>
