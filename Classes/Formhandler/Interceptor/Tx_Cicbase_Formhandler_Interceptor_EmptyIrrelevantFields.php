c46fd7ebdefdecc6d66ecfe3214d8d3652d60718
Lucas Thurston <lucas@castironcoding.com>
2013-04-03 10:02:02 -0700

    [FEATURE] New (very basic) formhandler interceptor for emptying irrelevant fields

---
+++ Classes/Formhandler/Interceptor/Tx_Cicbase_Formhandler_Interceptor_EmptyIrrelevantFields.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * <code>
+ * saveInterceptors.1.class = Tx_Cicbase_Formhandler_Interceptor_EmptyIrrelevantFields
+ * saveInterceptors.1.config.fields {
+ *			lead_source_search_engine {
+ *				unless = equals
+ *				unless {
+ *					field = lead_source
+ * 					value = SearchEngine
+ *				}
+ * 			}
+ * 		}
+ * }
+ * </code>
+ */
+class Tx_Cicbase_Formhandler_Interceptor_EmptyIrrelevantFields extends Tx_Formhandler_AbstractInterceptor {
+
+	/**
+	 * The main method called by the controller
+	 *
+	 * @return array The probably modified GET/POST parameters
+	 */
+	public function process() {
+		foreach($this->settings as $fieldName => $fieldConf) {
+			$fieldName = substr($fieldName,0,-1);
+			$this->gp[$fieldName] = $this->checkShouldEmpty($fieldConf, $this->gp) ? '' : $this->gp[$fieldName];
+		}
+		return $this->gp;
+	}
+
+	/**
+	 * Presently only checks the "unless" => "equals" condition type
+	 * @param $fieldconf
+	 * @param $gp
+	 */
+	private function checkShouldEmpty($fieldConf,$gp) {
+		if($fieldConf['unless'] == 'equals') {
+			return $this->checkShouldEmptyUnless($fieldConf['unless.'],$gp);
+		}
+	}
+
+	/**
+	 *
+	 */
+	private function checkShouldEmptyUnless($unlessConf, $gp) {
+		$field = $unlessConf['field'];
+		$value = $unlessConf['value'];
+
+		return $gp[$field] !== $value;
+	}
+}
+?>
