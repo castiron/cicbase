95318e25825ae839be31d79ecefbb34d7745e73c
Peter Soots <peter@castironcoding.com>
2013-04-12 09:20:13 -0700

    [FEATURE] Adding <cic:form.errors/> VH.

---
+++ Classes/ViewHelpers/Form/ErrorsViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Zach Davis <zach@castironcoding.com>, Cast Iron Coding
+ *  Lucas Thurston <lucas@castironcoding.com>, Cast Iron Coding
+ *  Gabe Blair <gabe@castironcoding.com>, Cast Iron Coding
+ *  Peter Soots <peter@castironcoding.com>, Cast Iron Coding
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * A replacement for Fluid's textfield viewhelper.
+ */
+class Tx_Cicbase_ViewHelpers_Form_ErrorsViewHelper extends Tx_Cicbase_ViewHelpers_Form_AbstractFormFieldViewHelper {
+
+	/**
+	 * Initialize the arguments.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerUniversalTagAttributes();
+	}
+
+	/**
+	 * We need to override this so that only the
+	 * errors are rendered.
+	 *
+	 * @return string
+	 */
+	protected function callRenderMethod() {
+		return $this->renderErrors();
+	}
+
+	/**
+	 * Nothing to do here; we're not rendering a
+	 * form element.
+	 *
+	 * @throws Exception
+	 */
+	public function render() {
+		throw new Exception("Render method not implemented for ErrorsViewHelper.");
+	}
+}
+?>
