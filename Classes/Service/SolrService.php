0fda859a9afe72cffe24fd0128e73e08f81bc3ea
Peter Soots <peter@castironcoding.com>
2013-11-18 15:01:14 -0800

    [FEATURE ORBEST #60640054] Created a SOLR Service class for managing SOLR searches.

---
+++ Classes/Service/SolrService.php
@@ -0,0 +1,380 @@
+<?php
+namespace CIC\Cicbase\Service;
+
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2013
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 3 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
+
+/**
+ *
+ *
+ * @package orbest
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class SolrService {
+	/**
+	 *        @var array
+	 */
+	protected $solrConnection = array(
+		'host' => '',
+		'port' => '',
+		'path' => '',
+		'scheme' => '',
+	);
+
+	/**
+	 * Set to true if a query was executed
+	 * @var boolean
+	 */
+	private $queryExecuted = false;
+
+	/**
+	 * Keywords, defaults to *
+	 * @var string
+	 */
+	private $keywords = '*';
+
+
+	/**
+	 * Solr response, set when the query is executed
+	 * @var mixed|object|boolean
+	 */
+	private $response = false;
+
+	/**
+	 * Solr search object, set when the query is executed
+	 * @var mixed|object|boolean
+	 */
+	private $search = false;
+
+	/**
+	 * Solr query object, set when the query is executed
+	 * @var mixed|object|boolean
+	 */
+	private $query = false;
+
+	/**
+	 * Solr The number of results found
+	 * @var mixed|integer|boolean
+	 */
+	private $numFound = false;
+
+	/**
+	 * Solr The number of results found
+	 * @var mixed|integer|boolean
+	 */
+	protected $queryLimit = 0;
+
+
+	/**
+	 * Solr The number of results found
+	 * @var mixed|integer|boolean
+	 */
+	protected $queryOffset = 0;
+
+
+	/**
+	 * The regex for text cleanup
+	 * @var string
+	 */
+	protected $textCleanupRegex = '/[^A-Za-z0-9]+/';
+
+	/**
+	 * The filters that are joined by AND to make a complete Query to Solr
+	 * @var array
+	 */
+	private $filters = array();
+
+	/**
+	 * @var TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser
+	 * @inject
+	 */
+	protected $typoscriptParser;
+
+
+	/**
+	 * Guesses the solr connection configuration (useful if called on page request)
+	 */
+	public function initialize($documentType = '') {
+		$this->addFilter("type:$documentType");
+	}
+
+	/**
+	 * Sets a single connection parameter or an array of parameters
+	 */
+	public function setSolrConnectionParameter() {
+		$args = func_get_args();
+		if(is_string($args[0]) && is_string($args[1])) {
+			$this->solrConnection[$args[0]] = $args[1];
+		} elseif(is_array($args[0])) {
+			foreach($args[0] as $k => $v) {
+				$this->solrConnection[(string) $k] = (string) $v;
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function addArgFilters($args) {
+		print_r(__FUNCTION__,func);
+		foreach($args as $argName => $argValue) {
+			$methodName = 'add'.ucfirst($argName).'Filter';
+			if(method_exists($this,$methodName)) {
+				$this->$methodName($args);
+			}
+		}
+		$this->postAddArgFilters($args);
+	}
+
+	/**
+	 *  If a filter has been applied
+	 *  @return boolean
+	 */
+	public function getHasFilters() {
+		return count($this->filters) ? true : false;
+	}
+
+	/**
+	 * @param string
+	 */
+	protected function addFilter($filter) {
+		$this->filters[] = $filter;
+	}
+
+	/**
+	 * @param $keywords
+	 */
+	public function setKeywords($keywords) {
+		$this->keywords = $keywords;
+	}
+
+	public function getKeywords() {
+		return $this->keywords;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function getFieldList() {
+		return array('*','score');
+	}
+
+	/**
+	 * The max number of results to return
+	 * @var integer
+	 */
+	protected function setQueryLimit($limit) {
+		$this->queryLimit = (integer) $limit;
+	}
+
+	/**
+	 * The max number of results to return
+	 * @return integer
+	 */
+	protected function getQueryLimit() {
+		return $this->queryLimit;
+	}
+
+	/**
+	 * The query offset
+	 * @var integer
+	 */
+	protected function setQueryOffset($offset) {
+		$this->queryOffset = (integer) $offset;
+	}
+
+	/**
+	 * The query offset
+	 * @return integer
+	 */
+	protected function getQueryOffset() {
+		return $this->queryOffset;
+	}
+
+	/**
+	 * Defined in a child class
+	 */
+	protected function postAddArgFilters() {
+	}
+
+	/**
+	 *
+	 */
+	protected function getHasSorting() {
+		return is_array($this->sorting) && count($this->sorting);
+	}
+
+
+	/**
+	 * This function creates a conjunction of string
+	 * values. You can pass it a list of strings or
+	 * an array of strings.
+	 *
+	 * Empty strings are skipped.
+	 *
+	 * @static
+	 * @return string Something like: "(a AND b AND c)"
+	 */
+	protected static function logicalAND(){
+		$values = func_get_args();
+		if(is_array($values[0])){
+			$values = $values[0];
+		}
+		return self::junction('AND', $values);
+	}
+
+	/**
+	 * This function creates a disjunction of string
+	 * values. You can pass it a list of strings or
+	 * an array of strings.
+	 *
+	 * Empty strings are skipped.
+	 *
+	 * @static
+	 * @return string Something like: "(a OR b OR c)"
+	 */
+	protected static function logicalOR(){
+		$values = func_get_args();
+		if(is_array($values[0])){
+			$values = $values[0];
+		}
+		return self::junction('OR', $values);
+	}
+
+	/**
+	 * @static
+	 * @param $timestamp
+	 * @return string
+	 */
+	protected static function timestampToDate($timestamp) {
+		return date("Y-m-d\TG:i:s\Z", $timestamp);
+	}
+
+	/**
+	 * This function makes a disjunction or a conjuncion
+	 * string. (i.e. "(a OR b OR c)" or "(d AND e)")
+	 *
+	 * @static
+	 * @param string $junction "AND" or "OR"
+	 * @param array $values An array of strings (atoms)
+	 * @return string
+	 */
+	protected static function junction($junction, array $values){
+		// remove empties
+		foreach($values as $val){
+			$parsed = explode(':',$val);
+			if($val !== '' && $parsed[1] !== ''){
+				$newVals[] = $val;
+			}
+		}
+
+		$values = $newVals;
+		$length = count($values);
+		if($length == 0){
+			return '';
+		}
+		if($length == 1){
+			return $values[0];
+		}
+
+		$str = '(';
+
+		for($i = 0; $i < $length; ++$i){
+			$str .= $values[$i];
+			if($i + 1 < $length) {
+				$str .= ' '.$junction.' ';
+			}
+		}
+
+		$str .= ') ';
+		return $str;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	private function executeQuery() {
+		$this->queryExecuted = true;
+
+		$solrConnection = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_ConnectionManager')->getConnection();
+		$search = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_Search', $solrConnection);
+		$query = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_Query', $this->getKeywords());
+
+		$query->setFieldList($this->getFieldList());
+
+		if(is_array($this->sorting)) {
+			$sortingDirection = strtolower($this->sorting[1] == 'asc' ? tx_solr_Query::SORT_ASC : tx_solr_Query::SORT_DESC);
+			$query->setSorting($this->sorting[0].' '.$sortingDirection);
+		}
+
+		$query->addFilter(self::logicalAND($this->filters));
+		$this->response = $search->search($query, $this->getQueryOffset(), $this->getQueryLimit());
+		$this->body = json_decode($this->response->getRawResponse());
+		$this->search = $search;
+		$this->query = $query;
+		$this->numFound = $this->body->response->numFound;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function getResultDocuments() {
+		$this->executeQuery();
+		return $this->search->getResultDocuments();
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function getNumFound() {
+		return $this->numFound;
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function getRecords() {
+		return $this->getResultDocuments();
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function getRecordUids() {
+		$out = array();
+		foreach($this->getResultDocuments() as $record) {
+			$out[] = $record->uid;
+		}
+		return $out;
+	}
+
+}
+
+?>
