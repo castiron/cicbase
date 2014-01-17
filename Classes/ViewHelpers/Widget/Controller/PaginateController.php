93a19b174c4a424c52263be62cba58f733834534
Peter Soots <peter@castironcoding.com>
2014-01-16 17:46:34 -0800

    [BUGFIX] In early versions of T3, the paginate VH template can't be overriden by other extensions. This fixes that for those versions.

--- Classes/ViewHelpers/Widget/Controller/PaginateController.php
+++ Classes/ViewHelpers/Widget/Controller/PaginateController.php
@@ -119,6 +119,24 @@ class Tx_Cicbase_ViewHelpers_Widget_Controller_PaginateController extends Tx_Flu
 
 		return $pagination;
 	}
+
+	/**
+	 * Allows the widget template root path to be overriden via the framework configuration,
+	 * e.g. plugin.tx_extension.view.widget.<WidgetViewHelperClassName>.templateRootPath
+	 *
+	 * @param Tx_Extbase_MVC_View_ViewInterface $view
+	 * @return void
+	 */
+	protected function setViewConfiguration(Tx_Extbase_MVC_View_ViewInterface $view) {
+		$extbaseFrameworkConfiguration = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+		$widgetViewHelperClassName = $this->request->getWidgetContext()->getWidgetViewHelperClassName();
+
+		if (isset($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath'])
+			&& strlen($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath']) > 0
+			&& method_exists($view, 'setTemplateRootPath')) {
+			$view->setTemplateRootPath(t3lib_div::getFileAbsFileName($extbaseFrameworkConfiguration['view']['widget'][$widgetViewHelperClassName]['templateRootPath']));
+		}
+	}
 }
 
 ?>
