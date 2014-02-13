473ced1f27b35cf184f00afdff6ae543a104ba31
Peter Soots <peter@castironcoding.com>
2014-02-13 14:25:54 -0800

    [CHORE] Upgrading ComplexIfViewHelper to work with T3 6.x

--- Classes/ViewHelpers/ComplexIfViewHelper.php
+++ Classes/ViewHelpers/ComplexIfViewHelper.php
@@ -1,18 +1,15 @@
 <?php
+namespace CIC\Cicbase\ViewHelpers;
 
-class Tx_Cicbase_ViewHelpers_ComplexIfViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
+class ComplexIfViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper {
 
 	/**
-	 * renders <f:then> child if $condition is true, otherwise renders <f:else> child.
-	 *
 	 * @param boolean $condition1
 	 * @param boolean $condition2
-	 * @param string $junction A string representation of a junction (i.e. 'and', '&&', 'or', or '||')
+	 * @param string $junction
 	 * @param boolean $condition3
-	 * @return string the rendered string
-	 * @author Sebastian Kurfürst <sebastian@typo3.org>
-	 * @author Bastian Waidelich <bastian@typo3.org>
-	 * @api
+	 * @return string
+	 * @throws Exception
 	 */
 	public function render($condition1, $condition2, $junction, $condition3 = null) {
 		$junction = strtolower($junction);
