be11b7dafdd51ae4558f2070fdace53987445d64
Peter Soots <peter@castironcoding.com>
2014-08-27 10:35:53 -0700

    [FEATURE] Adding an InjectionService.
    For use in places where dependency injection is not provided, like tests or tasks.

---
+++ Classes/Service/InjectionService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CIC\Cicbase\Service;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class InjectionService extends \TYPO3\CMS\Extbase\Object\Container\Container {
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManager
+	 */
+	protected $objectManager;
+
+	/**
+	 * A public wrapper for the protected injectDependencies function
+	 *
+	 * @param mixed $instance
+	 */
+	public function doInjection($instance) {
+		$className = get_class($instance);
+		$classInfo = $this->getClassInfo($className);
+		$this->injectDependencies($instance, $classInfo);
+	}
+
+
+	/**
+	 * Overriding this to let the REAL objectManager do the heavy lifting
+	 *
+	 * @param string $className
+	 * @param array $givenConstructorArguments
+	 * @return mixed|object
+	 */
+	protected function getInstanceInternal($className, $givenConstructorArguments = array()) {
+		if (!$this->objectManager) {
+			$this->objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		}
+		array_unshift($givenConstructorArguments, $className);
+		return call_user_func_array(array($this->objectManager, 'get'), $givenConstructorArguments);
+	}
+
+
+	/**
+	 * This method WAS private in parent class...
+	 *
+	 * @param string $className
+	 * @return \TYPO3\CMS\Extbase\Object\Container\ClassInfo
+	 */
+	protected function getClassInfo($className) {
+		$classNameHash = md5($className);
+		$classInfo = $this->getClassInfoCache()->get($classNameHash);
+		if (!$classInfo instanceof \TYPO3\CMS\Extbase\Object\Container\ClassInfo) {
+			$classInfo = $this->getClassInfoFactory()->buildClassInfoFromClassName($className);
+			$this->getClassInfoCache()->set($classNameHash, $classInfo);
+		}
+		return $classInfo;
+	}
+}
