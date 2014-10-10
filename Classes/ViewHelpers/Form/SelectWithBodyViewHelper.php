d94c2f9ceb0cb4cd2882465e050c4df39c3a3e82
Peter Soots <peter@castironcoding.com>
2014-10-09 17:13:40 -0700

    [FEATURE] Finally. A select VH that allows you to use you're own options.

---
+++ Classes/ViewHelpers/Form/SelectWithBodyViewHelper.php
@@ -0,0 +1,65 @@
+<?php
+namespace CIC\Cicbase\ViewHelpers\Form;
+
+
+class SelectWithBodyViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper {
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'select';
+
+
+	/**
+	 * Initialize arguments.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerUniversalTagAttributes();
+		$this->registerTagAttribute('multiple', 'string', 'if set, multiple select field');
+		$this->registerTagAttribute('optionCount', 'int', 'Required for multiple select');
+		$this->registerTagAttribute('size', 'string', 'Size of input field');
+		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
+		$this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this view helper', FALSE, 'f3-form-error');
+	}
+
+	/**
+	 * Render the tag.
+	 *
+	 * @return string rendered tag.
+	 * @throws
+	 */
+	public function render() {
+		$name = $this->getName();
+		if ($this->hasArgument('multiple')) {
+			if (!$this->hasArgument('optionCount')) {
+				throw new \Exception("You must specify an optionCount when using the 'multiple' argument for cic:selectWithBody");
+			}
+			$name .= '[]';
+		}
+		$this->tag->addAttribute('name', $name);
+		$this->tag->setContent($this->renderChildren());
+		$this->setErrorClassAttribute();
+		$content = '';
+		// register field name for token generation.
+		// in case it is a multi-select, we need to register the field name
+		// as often as there are elements in the box
+		if ($this->hasArgument('multiple') && $this->arguments['multiple'] !== '') {
+			$content .= $this->renderHiddenFieldForEmptyValue();
+			$optionCount = $this->arguments['optionCount'];
+			for ($i = 0; $i < $optionCount; $i++) {
+				$this->registerFieldNameForFormTokenGeneration($name);
+			}
+		} else {
+			$this->registerFieldNameForFormTokenGeneration($name);
+		}
+		$content .= $this->tag->render();
+		return $content;
+	}
+
+}
+
+
