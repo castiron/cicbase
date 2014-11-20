7fadef6b719a8d0d68ac96a43bd81688977c4ab4
Peter Soots <peter@castironcoding.com>
2014-11-19 16:21:11 -0800

    [FEATURE] Importing QueryProxy and AbstractRepository from work on sjcert

---
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -0,0 +1,138 @@
+<?php
+namespace CIC\Cicbase\Persistence;
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c)  TODO - INSERT COPYRIGHT
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 2 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
+use CIC\Cicbase\Utility\Arr;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+
+/**
+ * CIC\Cicbase\Persistence\PaginationRepository
+ *
+ * @version $Id$
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+abstract class BaseRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+
+	const LOGIC_AND = 'and';
+	const LOGIC_OR = 'or';
+
+	/**
+	 * @param callable $closure
+	 * @param string $combineWith
+	 * @return array|QueryResultInterface
+	 */
+	public function query(\Closure $closure, $combineWith = self::LOGIC_AND) {
+		$query = $this->createQuery();
+		$context = $this->objectManager->get('CIC\Cicbase\Persistence\QueryProxy', $query);
+		$result = $closure($context);
+		if ($result) return $result;
+		$combiner = 'logical'.ucfirst($combineWith);
+		$query->matching($query->$combiner($context->getContraints()));
+		return $query->execute();
+	}
+
+	/**
+	 * @param callable $closure
+	 * @param string $combineWith
+	 * @return array|QueryResultInterface
+	 */
+	public function count(\Closure $closure, $combineWith = self::LOGIC_AND) {
+		$query = $this->createQuery();
+		$context = $this->objectManager->get('CIC\Cicbase\Persistence\QueryProxy', $query);
+		$result = $closure($context);
+		if ($result) {
+			if ($result instanceof \Countable) {
+				return count($result);
+			} else if (is_object($result) && method_exists($result, 'count')) {
+				return $result->count();
+			} else {
+				return $result;
+			}
+		}
+		$combiner = 'logical'.ucfirst($combineWith);
+		$query->matching($query->$combiner($context->getContraints()));
+		return $query->count();
+	}
+
+	/**
+	 * Like query() but returns the raw array data and bypasses the property mapping
+	 * stages provided by extbase.
+	 *
+	 * @param callable $queryClosure
+	 * @param string $combineWith
+	 * @param null $keyField See \CIC\Cicbase\Arr::column
+	 * @param null $valueField See \CIC\Cicbase\Arr::column
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function rows(\Closure $queryClosure, $combineWith = self::LOGIC_AND, $keyField = NULL, $valueField = NULL) {
+		$result = $this->query($queryClosure, $combineWith);
+		$rows = $this->persistenceManager->getObjectDataByQuery($result->getQuery());
+
+		if ($keyField !== NULL || $valueField !== NULL) {
+			return Arr::column($rows, $valueField, $keyField);
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * Like rows() but works with an existing QueryResult.
+	 *
+	 * @param QueryResultInterface $result
+	 * @param null $keyField See \CIC\Cicbase\Arr::column
+	 * @param null $valueField See \CIC\Cicbase\Arr::column
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function rowsFromQueryResult(QueryResultInterface $result, $keyField = NULL, $valueField = NULL) {
+		$rows = $this->persistenceManager->getObjectDataByQuery($result->getQuery());
+
+		if ($keyField !== NULL || $valueField !== NULL) {
+			return Arr::column($rows, $valueField, $keyField);
+		}
+
+		return $rows;
+	}
+
+	/**
+	 * Finds objects by UID.
+	 *
+	 * @param array|string $uids
+	 * @return array|QueryResultInterface
+	 * @throws \Exception
+	 */
+	public function findByUids($uids) {
+		if(is_string($uids)) {
+			$uids = explode(',', $uids);
+		}
+		if (!is_array($uids) && !$uids instanceof \ArrayAccess && !$uids instanceof \Traversable) {
+			throw new \Exception('findByUids() only takes a comma separated string of uids or an array of uids');
+		}
+
+		return $this->query(function ($q) use ($uids) { $q->in('uid', $uids); });
+
+	}
+
+}
---
+++ Classes/Persistence/QueryProxy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace CIC\Cicbase\Persistence;
+
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
+class QueryProxy {
+
+	/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface */
+	protected $query;
+
+	/** @var array */
+	protected $constraints = [];
+
+	/**
+	 * @param QueryInterface $query
+	 */
+	public function __construct(QueryInterface $query) {
+		$this->query = $query;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getContraints() {
+		return $this->constraints;
+	}
+
+	/**
+	 * @param callable $closure
+	 */
+	public function logicalNot(\Closure $closure) {
+		/** @var QueryProxy $newProxy */
+		$newProxy = $this->newProxy();
+		$closure($newProxy);
+		$this->constraints[] = $this->query->logicalNot(current($newProxy->getContraints()));
+	}
+
+	/**
+	 * @param callable $closure
+	 */
+	public function logicalAnd(\Closure $closure) {
+		/** @var QueryProxy $newProxy */
+		$newProxy = $this->newProxy();
+		$closure($newProxy);
+		$this->constraints[] = $this->query->logicalAnd($newProxy->getContraints());
+	}
+
+	/**
+	 * @param callable $closure
+	 */
+	public function logicalOr(\Closure $closure) {
+		/** @var QueryProxy $newProxy */
+		$newProxy = $this->newProxy();
+		$closure($newProxy);
+		$this->constraints[] = $this->query->logicalOr($newProxy->getContraints());
+	}
+
+	/**
+	 * @param array $orderings
+	 */
+	public function setOrderings(array $orderings) {
+		$this->query->setOrderings($orderings);
+	}
+
+	/**
+	 * @param int $limit
+	 */
+	public function setLimit($limit) {
+		$this->query->setLimit($limit);
+	}
+
+	/**
+	 * @param int $offset
+	 */
+	public function setOffset($offset) {
+		$this->query->setOffset($offset);
+	}
+
+	/**
+	 * @param bool $bool
+	 */
+	public function setRespectStoragePage($bool) {
+		$settings = $this->query->getQuerySettings();
+		$settings->setRespectStoragePage($bool);
+		$this->query->setQuerySettings($settings);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	protected function newProxy() {
+		$class = get_class($this);
+		return new $class($this->query);
+	}
+
+	/**
+	 * Call the query object, but save the constraints
+	 *
+	 * @param string $name
+	 * @param array $args
+	 */
+	public function __call($name, $args) {
+		$this->constraints[] = call_user_func_array([$this->query, $name], $args);
+	}
+}
