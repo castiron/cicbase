f5e1b3f88ab686d4a51b2dec4ba41bbf038dfcea
Peter Soots <peter@castironcoding.com>
2015-04-08 12:47:01 -0700

    [CHORE] Fixes comment in c:unless VH

--- Classes/ViewHelpers/UnlessViewHelper.php
+++ Classes/ViewHelpers/UnlessViewHelper.php
@@ -5,7 +5,7 @@ namespace CIC\Cicbase\ViewHelpers;
 class UnlessViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper {
 
 	/**
-	 * renders <f:then> child if $condition is true, otherwise renders <f:else> child.
+	 * renders <f:then> child if $condition is FALSE, otherwise renders <f:else> child.
 	 *
 	 * @param boolean $condition View helper condition
 	 * @return string the rendered string
