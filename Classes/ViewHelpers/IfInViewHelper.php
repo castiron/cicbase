b9722d07f9ec3ae493773ca05132899d9a5a38e3
Peter Soots <peter@castironcoding.com>
2015-04-09 10:29:46 -0700

    [FEATURE] Expanding c:ifIn VH to allow arrays or CSV strings

--- Classes/ViewHelpers/IfInViewHelper.php
+++ Classes/ViewHelpers/IfInViewHelper.php
@@ -4,11 +4,11 @@ class Tx_Cicbase_ViewHelpers_IfInViewHelper extends Tx_Fluid_Core_ViewHelper_Abs
 
 	/**
 	 * @param string $value
-	 * @param string $list
+	 * @param array|string $list
 	 * @return string
 	 */
 	public function render($value, $list) {
-		if (t3lib_div::inList($list, $value)) {
+		if (is_array($list) ? in_array($value, $list) : t3lib_div::inList($list, $value)) {
 			return $this->renderThenChild();
 		} else {
 			return $this->renderElseChild();
