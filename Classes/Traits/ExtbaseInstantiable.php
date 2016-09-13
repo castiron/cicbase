bd6070a7f4a2db31d0ea1aeae1888cdba6fe50bd
Gabe Blair <gabe@castironcoding.com>
2016-09-12 17:06:49 -0700

    [B] Make ExtbaseInstantiable work in TYPO3 4

--- Classes/Traits/ExtbaseInstantiable.php
+++ Classes/Traits/ExtbaseInstantiable.php
@@ -11,9 +11,12 @@ trait ExtbaseInstantiable {
      * @return object
      */
     public static function get() {
-        $params = func_get_args();
-        array_unshift($params, static::class);
-        return call_user_func_array([static::objectManager(), 'get'], $params);
+        $args = func_get_args();
+        array_unshift($args, get_called_class());
+        return call_user_func_array(
+            array(static::objectManager(), 'get'),
+            $args
+        );
     }
 
     /**
