c0a2c7c19e40690ded357c00c929070fc8d3962f
Gabe Blair <gabe@castironcoding.com>
2016-10-27 17:47:37 -0700

    [B] Minor linting

--- Classes/Domain/Model/AbstractArrayBasedModel.php
+++ Classes/Domain/Model/AbstractArrayBasedModel.php
@@ -13,6 +13,7 @@ class AbstractArrayBasedModel {
     /**
      * @param $name
      * @param $arguments
+     * @return mixed|void
      */
     public function __call($name, $arguments) {
         /**
