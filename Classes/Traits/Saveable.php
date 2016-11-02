53e4cac7268bdad8ef78ef3f08f652ef8482c8f3
Gabe Blair <gabe@castironcoding.com>
2016-11-01 23:21:39 -0700

    [F] Add Saveable interface for array-based models

---
+++ Classes/Traits/Saveable.php
@@ -0,0 +1,39 @@
+<?php namespace CIC\Cicbase\Traits;
+
+/**
+ * Class Saveable
+ * @package CIC\Cicbase\Traits
+ */
+trait Saveable {
+    use Database;
+
+    public function save() {
+        $query = static::UPSERTquery(static::$table, $this->saveFields());
+        static::db()->sql_query($query);
+    }
+
+    /**
+     *
+     */
+    public function destroy() {
+        static::db()->exec_DELETEquery(static::$table, 'uid=' . intval($this->rec['uid']));
+    }
+
+    /**
+     * @param $uid
+     * @return array|FALSE|NULL
+     */
+    public function fetch($uid) {
+        if (!$uid) {
+            return;
+        }
+        return static::db()->exec_SELECTgetSingleRow('*', static::$table, 'uid=' . intval($uid));
+    }
+
+    /**
+     * @return array
+     */
+    protected function saveFields() {
+        return [];
+    }
+}
