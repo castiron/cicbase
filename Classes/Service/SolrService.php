3ae2990237cf4c828056e4f7762dfad7be23c7f2
Gabe Blair <gabe@castironcoding.com>
2017-01-22 21:10:42 -0800

    [F] Enable solr-native access control in solorservice queries

--- Classes/Service/SolrService.php
+++ Classes/Service/SolrService.php
@@ -54,6 +54,10 @@ class SolrService {
 	 */
 	private $keywords = '*';
 
+    /**
+     * @var array
+     */
+	protected $userAccessGroups = [];
 
 	/**
 	 * Solr response, set when the query is executed
@@ -356,10 +360,13 @@ class SolrService {
 
 		$solrConnection = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_ConnectionManager')->getConnection();
 		$search = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_Search', $solrConnection);
+		/** @var \Tx_Solr_Query $query */
 		$query = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_solr_Query', $this->getKeywords());
 		$solrConfiguration = \tx_solr_Util::getSolrConfiguration();
 
 		$query->setFieldList($this->getFieldList());
+        $query->setUserAccessGroups($this->userAccessGroups);
+
 
 		if($this->hasSorting()) {
 			$sortArray = $this->sorting;
@@ -413,6 +420,16 @@ class SolrService {
 		$this->executeQuery();
 		return $this->search->getResultDocuments();
 	}
+
+    /**
+     * @param array $groups
+     */
+    public function setUserAccessGroups($groups = []) {
+        if (!is_array($groups)) {
+            return;
+        }
+        $this->userAccessGroups = $groups;
+    }
 
 	/**
 	 *
