2387195b5d0443ef10662ab7a5461d622c8b5829
Gabe Blair <gabe@castironcoding.com>
2017-01-25 12:09:31 -0800

    [B] Fix inconsistent decimal count in cli mem. reporting

--- Classes/Traits/Reporting/CliReportable.php
+++ Classes/Traits/Reporting/CliReportable.php
@@ -30,7 +30,7 @@ trait CliReportable {
         $base = log($size) / log(1024);
         $suffix = array("", "KB", "MB", "GB", "TB");
         $f_base = floor($base);
-        return round(pow(1024, $base - floor($base)), 1) . $suffix[$f_base];
+        return number_format(round(pow(1024, $base - floor($base)), 1), 1) . $suffix[$f_base];
     }
 
     /**
