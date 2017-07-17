24e7ee6a5d01a1b3d45fceba2d4973db7e008070
Gabe Blair <gabe@castironcoding.com>
2017-07-17 14:46:37 -0700

    [F] Add be user presence check method

--- Classes/Traits/BackendUserable.php
+++ Classes/Traits/BackendUserable.php
@@ -8,10 +8,18 @@ trait BackendUserable {
     /**
      * @return \TYPO3\CMS\Backend\FrontendBackendUserAuthentication|null
      */
-    public function initBackendUser() {
+    public static function initBackendUser() {
         if (!$GLOBALS['BE_USER']) {
             $GLOBALS['BE_USER'] = $GLOBALS['TSFE']->initializeBackendUser();
         }
         return $GLOBALS['BE_USER'] ?: null;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function backendUserIsAuthenticated() {
+        static::initBackendUser();
+        return $GLOBALS['BE_USER'] ? true : false;
     }
 }
