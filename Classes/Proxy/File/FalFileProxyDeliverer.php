f06f3d71dc5016cd6878761916cbb71e91720e10
Gabe Blair <gabe@castironcoding.com>
2018-03-29 11:17:39 -0700

    [B] Fix issue when proxying huge files

--- Classes/Proxy/File/FalFileProxyDeliverer.php
+++ Classes/Proxy/File/FalFileProxyDeliverer.php
@@ -28,8 +28,40 @@ class FalFileProxyDeliverer extends FileProxyDeliverer {
         HttpHeaderUtility::sendHeaders(array_merge(
             $headers, static::fileHeaders($file)
         ));
-        readfile($path);
+
+        static::readfile($path);
+
         exit;
+    }
+
+    /**
+     * RE: huge files: PHP handles huge files with `readfile` no problemo, but if output buffering is on you can run
+     * into probs.
+     *
+     * This wrapper makes sure there is no output buffering going on (which would cause the entire delivered file
+     * to be loaded into the buffer, potentially surpassing memory_limit).
+     *
+     * @param $path
+     */
+    protected static function readfile($path)
+    {
+        static::clearBuffer();
+        readfile($path);
+    }
+
+    /**
+     *
+     */
+    protected static function clearBuffer() {
+        /**
+         * We use a static level rather than calling ob_get_level() each time (which _should_ decrement)
+         * for safety's sake.
+         */
+        $depth = ob_get_level();
+        while ($depth) {
+            ob_end_clean();
+            $depth--;
+        }
     }
 
     /**
