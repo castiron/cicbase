0c5389577e8cad309a7b74548ec78d5ed64fdfe9
Lucas Thurston <lucas@castironcoding.com>
2018-10-10 14:58:27 -0700

    [C] Modernize ExtbaseInstantiable trait

--- Classes/Traits/ExtbaseInstantiable.php
+++ Classes/Traits/ExtbaseInstantiable.php
@@ -1,6 +1,7 @@
 <?php namespace CIC\Cicbase\Traits;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 /**
  * Class ExtbaseInstantiable
@@ -13,17 +14,8 @@ trait ExtbaseInstantiable {
     public static function get() {
         $args = func_get_args();
         array_unshift($args, get_called_class());
-        return call_user_func_array(
-            array(static::objectManager(), 'get'),
-            $args
-        );
-    }
-
-    /**
-     * @return \TYPO3\CMS\Extbase\Object\ObjectManager object
-     */
-    protected static function objectManager() {
-        return GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+        return $objectManager->get(...$args);
     }
 }
 
