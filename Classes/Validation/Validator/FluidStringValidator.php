9b4be8d84087ca8a4c2d75233b65d52eb3994731
Lucas Thurston <lucas@castironcoding.com>
2019-08-27 12:11:09 -0700

    [C] Fix email templates for TYPO3 v8

--- Classes/Validation/Validator/FluidStringValidator.php
+++ Classes/Validation/Validator/FluidStringValidator.php
@@ -2,6 +2,13 @@
 namespace CIC\Cicbase\Validation\Validator;
 
 
+use TYPO3\CMS\Backend\View\BackendTemplateView;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContext;
+use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperResolver;
+use TYPO3Fluid\Fluid\View\AbstractView;
+
 /**
  * Class FluidStringValidator
  * @package CIC\Cicbase\Validation\Validator
@@ -31,6 +38,10 @@ class FluidStringValidator extends \TYPO3\CMS\Extbase\Validation\Validator\Abstr
 	 */
 	protected function isValid($value) {
 		try {
+		    $view = GeneralUtility::makeInstance(StandaloneView::class);
+		    $renderingContext = GeneralUtility::makeInstance(RenderingContext::class, $view);
+		    $renderingContext->setViewHelperResolver(GeneralUtility::makeInstance(ViewHelperResolver::class));
+		    $this->parser->setRenderingContext($renderingContext);
 			$this->parser->parse($value);
 		} catch (\Exception $e) {
 			$this->addError("Could not parse fluid template: ".$e->getMessage(), 1418149684);
