37450fb0650bc1aef04d111a5b7beb50540db3e0
Kyle Pratuch <kyle@castironcoding.com>
2025-01-31 16:48:19 -0800

    [B] Fix cache identifier error

--- Classes/Domain/Repository/FileRepository.php
+++ Classes/Domain/Repository/FileRepository.php
@@ -106,7 +106,7 @@ class FileRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 
 	protected function getCacheKey($key = '')
 	{
-		return 'heldFile_' . $GLOBALS['TSFE']->fe_user->id . $key;
+		return 'heldFile_'.$GLOBALS['TSFE']->fe_user->user['uid'].$key;
 	}
 
 	/**
@@ -131,7 +131,7 @@ class FileRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 	public function clearHeld($key = '')
 	{
 		$cache = $this->getCache();
-		$cache->remove($this->getCacheKey($key));
+		if($this->getHeld($key)) $cache->remove($this->getCacheKey($key));
 	}
 
 	/**
